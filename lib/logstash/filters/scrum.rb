9ff30fda6a711d9746af0fc490dbca7c5de6c689
Khan <Mohammed.Khan3@vce.com>
2016-08-15 15:11:48 -0500

    logic for yesterday's weather fixed

--- lib/logstash/filters/scrum.rb
+++ lib/logstash/filters/scrum.rb
@@ -6,43 +6,62 @@ require "logstash/namespace"
 # message field with whatever you specify in the configuration.
 #
 # It is only intended to be used as an example.
-class LogStash::Filters::Example < LogStash::Filters::Base
+class LogStash::Filters::Scrum < LogStash::Filters::Base
 
   # Setting the config_name here is required. This is how you
   # configure this filter from your Logstash config.
   #
   # filter {
-  #   example {
-  #     message => "My message..."
+  #   scrum {
+  #     weather => ["fieldname"]
   #   }
   # }
   #
-  config_name "example"
+  config_name "scrum"
 
   # Replace the message with this value.
-  config :message, :validate => :string, :default => "Hello World!"
+  config :weather, :validate => :array
 
-
-  public
   def register
     # Add instance variables
   end # def register
-
-  public
+  
   def filter(event)
-
-    if @message
-      # Replace the event message with our message as configured in the
-      # config file.
-
-      # using the event.set API
-      event.set("message", @message)
-      # correct debugging log statement for reference
-      # using the event.get API
-      @logger.debug? && @logger.debug("Message is now: #{event.get("message")}")
-    end
-
-    # filter_matched should go in the last line of our successful code
-    filter_matched(event)
+    
+  weather(event) if @weather
+  filter_matched(event)
   end # def filter
+    
+   
+   
+    $i=0
+    $j=0
+    $k=0
+    $temp = Array.new()
+    $metrics = Array.new()
+  def weather(event)
+    @weather.each do |field|
+      $i = ($i + 1) 
+      $temp[$i] = event[field]
+     # field = field + 1
+     end # end do 
+     
+     while $j < $i 
+        $j = ($j + 1)
+        x = $temp[$j-3].to_f
+        y = $temp[$j-2].to_f
+        z = $temp[$j-1].to_f
+        l = (x+y+z)/3
+        $metrics[$j] = l
+     end
+     
+     @weather.each do |field|
+      $k +=1
+      result = $metrics[$k] 
+      event[field] = result
+    end # end  @weather.each do    
+  end # end weather(event)
+    
+    
+    
 end # class LogStash::Filters::Example
