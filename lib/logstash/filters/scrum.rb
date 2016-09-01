2cfa89f4385724c023412025311898335e9e78c3
Khan <Mohammed.Khan3@vce.com>
2016-09-01 14:57:15 -0500

    commit

--- lib/logstash/filters/scrum.rb
+++ lib/logstash/filters/scrum.rb
@@ -21,7 +21,7 @@ class LogStash::Filters::Scrum < LogStash::Filters::Base
 
   # Replace the message with this value.
   config :weather, :validate => :array
-
+  public
   def register
     # Add instance variables
   end # def register
@@ -31,34 +31,48 @@ class LogStash::Filters::Scrum < LogStash::Filters::Base
   weather(event) if @weather
   filter_matched(event)
   end # def filter
-    
-   
-   
+  
+  private  
     $i=0
     $j=0
     $k=0
     $temp = Array.new()
     $metrics = Array.new()
+   
+   
   def weather(event)
+    
     @weather.each do |field|
-      $i = ($i + 1) 
+      $i += 1 
       $temp[$i] = event[field]
-     # field = field + 1
      end # end do 
      
      while $j < $i 
-        $j = ($j + 1)
+       $j += 1
+       if $j==1
+         result=0
+         $metrics[$j] = result
+     elsif $j==2
+         result=$temp[1].to_f
+         $metrics[$j] = result
+     elsif $j==3
+         x=$temp[1].to_f
+         y=$temp[2].to_f
+         result = (x+y)/2
+         $metrics[$j] = result
+     else
         x = $temp[$j-3].to_f
         y = $temp[$j-2].to_f
         z = $temp[$j-1].to_f
-        l = (x+y+z)/3
-        $metrics[$j] = l
-     end
+        result = (x+y+z)/3
+        $metrics[$j] = result
+     end # end if
+     end # end while
      
      @weather.each do |field|
-      $k +=1
+      $k += 1
       result = $metrics[$k] 
-      event[field] = result
+      event[field] = result.to_f
     end # end  @weather.each do    
   end # end weather(event)
     
